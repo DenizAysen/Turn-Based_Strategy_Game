1470212bfd56c64c4e3ff9d52cfcdf8dd25fb750
Turgut Deniz Aysen <45630354+DenizAysen@users.noreply.github.com>
2023-10-25 15:33:58 +0300

    Unit Action System Init

--- Assets/Scripts/Unit Scripts/Unit.cs
+++ Assets/Scripts/Unit Scripts/Unit.cs
@@ -7,39 +7,43 @@ public class Unit : MonoBehaviour
 
     #region Serialized Fields
 
-    [SerializeField] private float stoppingDistance; 
+    [SerializeField] private float stoppingDistance;
+    [SerializeField] private Animator unitAnimator;
 
     #endregion
 
     #region Privates
 
     private Vector3 _targetPosition;
-    private Vector3 _moveDirection; 
-
+    private Vector3 _moveDirection;
+    private float _rotateSpeed = 10f;
     #endregion
 
+    private void Awake()
+    {
+        _targetPosition = transform.position;
+    }
     private void Update()
     {
+
         if (Vector3.Distance(transform.position, _targetPosition) > stoppingDistance)
         {
             _moveDirection = (_targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += _moveDirection * moveSpeed * Time.deltaTime;
+            transform.forward = Vector3.Lerp(transform.forward,_moveDirection,Time.deltaTime * _rotateSpeed);
+            unitAnimator.SetBool("IsWalking", true);
         }
+        else 
+            unitAnimator.SetBool("IsWalking", false);
         
         //if (Input.GetKeyDown(KeyCode.T))
         //{ 
         //    Move(new Vector3(4, 0, 4));
-        //}
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("Fareye basti");
-            Debug.Log("konum : " + MouseWorld.GetPosition());
-            Move(MouseWorld.GetPosition());
-        }
+        
             
     }
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         _targetPosition = targetPosition;
        // _targetPosition = new Vector3(_targetPosition.x, 0, _targetPosition.z);
